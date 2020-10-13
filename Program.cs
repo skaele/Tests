9c21c51edbbb300958d85927f73f8fd3e534e283
skaele <50191467+skaele@users.noreply.github.com>
2020-10-13 15:07:53 +0300

    Test task for MTC

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MTC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfList = int.Parse(Console.ReadLine());
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < sizeOfList; i++) list.Add(int.Parse(Console.ReadLine()));
+
+            int value = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            foreach (var item in IndexOfValue(list, value)) Console.WriteLine(item);
+        }
+
+        private static List<int> IndexOfValue(List<int> list, int value) 
+        {
+            List<int> output = new List<int>();
+            for (int index = 0; index < list.Count; index++)
+            {
+                if (list[index] == value) output.Add(index);
+            }
+            return output;
+        }
+    }
+}
